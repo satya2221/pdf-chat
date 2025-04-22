6eb066ec072189380f6420a4d49e02159a3d05a4
satya2221 <satyaghifari12@gmail.com>
2025-04-22 20:26:32 +0700

    Upload Pdf process

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from .models import Document
 
 # Create your views here.
+class DocumentUploadView(View):
+    def get(self, request):
+        return render(request, "documents/index.html")
+    
+    def post(self, request):
+        file = request.FILES.get("file")
+
+        try:
+            Document.objects.create(file=file, name=file.name)
+        except Exception as e:
+            print(e)
+
+        return redirect("documents")
