caba07b835a2d57643c9771352270c122de76f63
satya2221 <satyaghifari12@gmail.com>
2025-04-22 21:36:39 +0700

    OCR uploaded pdf

--- core/ai/mistral.py
+++ core/ai/mistral.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 
-client = Mistral(api_key=MISTRAL_API_KEY)
+mistral = Mistral(api_key=MISTRAL_API_KEY)
--- documents/views.py
+++ documents/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Document
+from core.ai.mistral import mistral
 
 # Create your views here.
 class DocumentUploadView(View):
@@ -11,7 +12,28 @@ class DocumentUploadView(View):
         file = request.FILES.get("file")
 
         try:
-            Document.objects.create(file=file, name=file.name)
+            document = Document.objects.create(file=file, name=file.name)
+
+            uploaded_pdf = mistral.files.upload(
+                file={
+                    "file_name": document.name,
+                    "content": open(f"media/documents/{document.name}", "rb")
+                },
+                purpose="ocr"
+            )
+
+            signed_url = mistral.files.get_signed_url(file_id=uploaded_pdf.id)
+            print(signed_url)
+
+            ocr_result = mistral.ocr.process(
+                model="mistral-ocr-latest",
+                document={
+                    "type":"document_url",
+                    "document_url": signed_url.url
+                }
+            )
+            print(ocr_result)
+
         except Exception as e:
             print(e)
 
